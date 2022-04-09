18c04c1a5c12484b13451cbb22d30ee0b2c0dddf
lordofprograms <shelegmike@gmail.com>
2022-04-09 11:46:41 +0300

    Add hand bent tracking files

---
+++ HandTrackingModule.py
@@ -0,0 +1,220 @@
+import math
+import cv2
+import mediapipe as mp
+import time
+
+
+class HandDetector:
+    def __init__(self, mode=False, maxHands=1, detectionCon=0.7, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+        self.tip_ids = [4, 8, 12, 16, 20]
+        self.name_mappings = {0: "thumb", 1: "index", 2: "middle", 3: "ring", 4: "little"}
+        self.lm_list = []
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmlist = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for tid, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print("Z axis = ", lm.z, int(lm.z * w))
+                lmlist.append([tid, cx, cy, lm.z])
+                if draw:
+                    cv2.putText(img, str(tid), (cx, cy), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (0, 255, 0), 2)
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
+        return lmlist
+
+    def find_raw_position(self, img, hand_no=0, draw=True):
+        lmlist = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[hand_no]
+            for tid, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print("Z axis = ", lm.z, int(lm.z * w))
+                lmlist.append([tid, lm.x, lm.y, lm.z])
+                if draw:
+                    cv2.putText(img, str(tid), (cx, cy), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (0, 255, 0), 2)
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
+        return lmlist
+
+    def fingers_status(self, lm_list):
+        self.lm_list = lm_list
+
+        fingers = []
+        if self.results.multi_hand_landmarks:
+            myHandType = self.hand_type()
+            fingers.append(0)
+
+            # self.mpDraw.plot_landmarks(
+            #     lm_list, self.mpHands.HAND_CONNECTIONS, azimuth=5)
+
+            # # Thumb
+            # if myHandType == "Right":
+            #     if self.lm_list[self.tip_ids[0]][0] > self.lm_list[self.tip_ids[0] - 1][0]:
+            #         fingers.append(1)
+            #     else:
+            #         fingers.append(0)
+            # else:
+            #     if self.lm_list[self.tip_ids[0]][0] < self.lm_list[self.tip_ids[0] - 1][0]:
+            #         fingers.append(1)
+            #     else:
+            #         fingers.append(0)
+
+            print("index_points = ", [self.lm_list[8][1:], self.lm_list[7][1:], self.lm_list[6][1:], self.lm_list[5][1:]])
+            print("middle_points = ", [self.lm_list[12][1:], self.lm_list[11][1:], self.lm_list[10][1:], self.lm_list[9][1:]])
+            print("ring_points = ", [self.lm_list[16][1:], self.lm_list[15][1:], self.lm_list[14][1:], self.lm_list[13][1:]])
+            print("little_points = ", [self.lm_list[20][1:], self.lm_list[19][1:], self.lm_list[18][1:], self.lm_list[17][1:]])
+            print("lowest point = ", [self.lm_list[0][1:]])
+
+            # thumb and 4 Fingers
+            for tip_id in range(0, 5):
+                # if self.tip_ids[tip_id] == 8:
+                #     if self.lm_list[8][1] <= self.lm_list[7][1]:
+                #         print("Second index point is higher!")
+
+                # # using palm base point and tip and base points of the finger
+                # bent_percent = self.__radian_slope2percent(self.lm_list[self.tip_ids[tip_id]][1:],
+                #                                            self.lm_list[self.tip_ids[tip_id - 3]][1:],
+                #                                            self.lm_list[0][1:])
+
+                # # using 1st, 3rd and 4th points of the finger
+                # bent_percent = self.__radian_slope2percent(self.lm_list[self.tip_ids[tip_id]][1:],
+                #                                            self.lm_list[self.tip_ids[tip_id - 2]][1:],
+                #                                            self.lm_list[self.tip_ids[tip_id - 3]][1:])
+
+                # # using sequence finger segments calc
+                # bent_percent = self.__finger_bent_percent([self.lm_list[tip_id - i][1:] for i in range(0, 4)])
+                bent_percent = self.__finger_bent_percent([self.lm_list[self.tip_ids[tip_id]][1:],
+                                                           self.lm_list[self.tip_ids[tip_id - 1]][1:],
+                                                           self.lm_list[self.tip_ids[tip_id - 2]][1:],
+                                                           self.lm_list[self.tip_ids[tip_id - 3]][1:]])
+                print(f"{self.name_mappings.get(tip_id)} finger bent percent = {round(bent_percent, 2)} %")
+
+                # if self.lm_list[self.tip_ids[tip_id]][1] < self.lm_list[self.tip_ids[tip_id] - 2][1]:
+                #     fingers.append(1)
+                # else:
+                #     fingers.append(0)
+        return fingers
+
+    def __finger_bent_percent(self, finger_points):
+        v1, v2, v3, v4 = finger_points
+        m1, m2, m3 = self.__slope(v1, v2), self.__slope(v2, v3), self.__slope(v3, v4)
+        first_angle, second_angle = self.__slope_angle(m1, m2), self.__slope_angle(m2, m3)
+        print(first_angle, second_angle)
+        angle_sum = first_angle + second_angle
+        angle_sum = 90 if angle_sum > 90 else angle_sum
+        return (1 - math.sin((angle_sum / 180) * math.pi)) * 100
+
+    def __radian_slope2percent(self, v1, v2, v3):
+        m1 = self.__slope(v1, v2)
+        m2 = self.__slope(v2, v3)
+        if m1 == 0 or m2 == 0:
+            print("slope is zero")
+            return 0
+        else:
+            radian = self.__slope_radian(m1, m2)
+            # print("not zero", (radian * 180) / math.pi,  math.sin(radian), (1 - math.sin(radian)) * 100, "%")
+            # return (1 - math.sin(radian)) * 100
+            # TODO try add for some fingers logic with 1 - x and select math function
+            print(math.cos(radian), math.sin(radian), math.tan(radian), math.tanh(radian))
+            return math.cos(radian) * 100
+
+    @staticmethod
+    def __slope(v1, v2):
+        try:
+            return (v2[1] - v1[1]) / (v2[0] - v1[0])
+        except ZeroDivisionError:
+            print("slope error")
+            return 0
+
+    @staticmethod
+    def __slope_angle(m1, m2):
+        # Store the tan value  of the angle
+        angle = abs((m2 - m1) / (1 + m1 * m2))
+
+        # Calculate tan inverse of the angle
+        rad = math.atan(angle)
+
+        # Convert the angle from
+        # radian to degree
+        val = (rad * 180) / math.pi
+
+        # Print the result
+        return val
+
+    @staticmethod
+    def __slope_radian(m1, m2):
+        # Store the tan value  of the angle
+        angle = abs((m2 - m1) / (1 + m1 * m2))
+
+        # Calculate tan inverse of the angle
+        rad = math.atan(angle)
+
+        # Convert the angle from
+        # radian to degree
+        # val = (rad * 180) / math.pi
+
+        # Print the result
+        return rad
+
+    def hand_type(self):
+        """
+        Check whether the incoming hand is left or right
+        : return: "Right" or "Left"
+        """
+        if self.results.multi_hand_landmarks:
+            if self.lm_list[17][0] < self.lm_list[5][0]:
+                return "Right"
+            else:
+                return "Left"
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmlist = detector.findPosition(img)
+        if len(lmlist) != 0:
+            detector.fingers_status(lmlist)
+            # print(fingers_status)
+            # print(lmlist[4])
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+# if __name__ == "__main__":
+#     main()
---
+++ hand_bent_status.py
@@ -0,0 +1,158 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+
+wCam, hCam = 640, 480
+cap = cv2.VideoCapture(0)
+pTime = 0
+
+detector = htm.HandDetector(detectionCon=0.75, trackCon=0.75)
+
+# TODO add list with 5 latest or one value, if next value is bigger or smaller for 7-10 update value
+# TODO also make this for ref_points
+
+
+class HandBentStatus:
+    def __init__(self):
+        self.minVal = 0
+        self.maxVal = 100
+
+        # if needed can change middle to little finger base, 9 to 17
+        self.thumb_finish_fb = 9
+        self.min_tip_dict = {4: 0.1, 8: 1.7, 12: 1.5, 16: 1.7, 20: 2}
+        self.max_tip_dict = {4: 4}
+
+        # TODO move to class for normal usage of heuristic for checking diff and make less updates
+        vals_amount = 5
+        self.prev_len_arr = [0.0] * vals_amount
+        self.prev_percent_arr = [0.0] * vals_amount
+        self.prev_ref_point = 0.0
+        self.name_mappings = {0: "thumb", 1: "index", 2: "middle", 3: "ring", 4: "little"}
+
+    def selected_tip_demo(self, lmList, tip_id: int = 8):
+        # tip_id = 8
+        x1, y1 = lmList[tip_id][1], lmList[tip_id][2]
+        if tip_id != 4:
+            x2, y2 = lmList[tip_id - 3][1], lmList[1][2]
+        else:
+            x2, y2 = lmList[self.thumb_finish_fb][1], lmList[self.thumb_finish_fb][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        # print(f"length = {length}, prev length was ", prev_len)
+        # prev_len = length
+
+        # length_arr.append(length)
+        # print(f"min: {np.amin(length_arr)}, max: {np.amax(length_arr)} and arr size = {len(length_arr)}")
+
+        # print(length)
+
+        # Hand range 50 - 300
+        # Volume Range -65 - 0
+
+        # rel_hand_max_range = (abs(lmList[tip_id][2] - lmList[tip_id - 2][2]) +
+        #                       abs(lmList[1][2] - lmList[tip_id - 3][2])) * 1.3
+        # rel_hand_min_range = abs(lmList[tip_id - 1][2] - lmList[tip_id - 2][2]) * 2
+
+        # print("range min/max", rel_hand_min_range, rel_hand_max_range)
+
+        ref_point = abs(lmList[2][2] - lmList[1][2])
+        # print(f"ref point = {ref_point}, prev ref point was {prev_ref_point}")
+        # prev_ref_point = ref_point
+
+        rel_hand_max_range = ref_point * self.max_tip_dict.get(tip_id, 5.5)
+        rel_hand_min_range = ref_point * self.min_tip_dict.get(tip_id)
+
+        # # config for small finger
+        # rel_hand_max_range = ref_point * 5.5
+        # rel_hand_min_range = ref_point * 2
+
+        vol = np.interp(length, [rel_hand_min_range, rel_hand_max_range], [self.minVal, self.maxVal])
+        volBar = np.interp(length, [rel_hand_min_range, rel_hand_max_range], [400, 150])
+        # volPer = np.interp(length, [rel_hand_min_range, rel_hand_max_range], [0, 100])
+        volPer = vol
+        print("experiment: ", int(length), vol, lmList[tip_id][3])
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+        cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+        cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+        cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
+                    1, (255, 0, 0), 3)
+
+    def fingers_bent_percent(self, lm_list) -> list:
+        # TODO add thumb drive
+        ref_point = abs(lm_list[2][2] - lm_list[1][2])
+        # print(f"test point finger base = {abs(lmList[5][1] - lmList[17][1])} and palm base test = "
+        #       f"{abs(lmList[17][2] - lmList[0][2])}")
+        # TODO maybe do something when we show front side of the palm
+        new_prev_diff = abs(ref_point - self.prev_ref_point)
+        if new_prev_diff > 30:
+            print(f"ref point = {ref_point}, prev ref point was {self.prev_ref_point}, |diff| = {new_prev_diff}")
+            # TODO test it more to make it more stable
+            self.prev_ref_point = ref_point
+            # print("ref point size = ", ref_point)
+        else:
+            ref_point = self.prev_ref_point
+
+        fingers_percent = []
+        tip_id_list = [4, 8, 12, 16, 20]
+        for i, tip_id in enumerate(tip_id_list):
+            x1, y1 = lm_list[tip_id][1], lm_list[tip_id][2]
+
+            if tip_id != 4:
+                x2, y2 = lm_list[tip_id - 3][1], lm_list[1][2]
+            else:
+                x2, y2 = lm_list[self.thumb_finish_fb][1], lm_list[self.thumb_finish_fb][2]
+
+            length = math.hypot(x2 - x1, y2 - y1)
+            # print(f"length = {length}, prev len was {self.prev_len_arr[i]}, diff = {length - self.prev_len_arr[i]}")
+            # self.prev_len_arr[i] = length
+
+            # TODO maybe use another ref points or another solution for calculating relation values
+            rel_hand_max_range = ref_point * self.max_tip_dict.get(tip_id, 5.5)
+            rel_hand_min_range = ref_point * self.min_tip_dict.get(tip_id)
+
+            open_percent = np.interp(length, [rel_hand_min_range, rel_hand_max_range], [self.minVal, self.maxVal])
+            bent_percent = round(100 - open_percent)
+
+            # TODO test it
+            if abs(bent_percent - self.prev_percent_arr[i]) > 5:
+                print(f"{self.name_mappings.get(i)}({i}) bent % ={bent_percent}, prev % was {self.prev_percent_arr[i]},"
+                      f"diff = {abs(bent_percent - self.prev_percent_arr[i])}")
+                self.prev_percent_arr[i] = bent_percent
+            else:
+                bent_percent = self.prev_percent_arr[i]
+
+            fingers_percent.append(bent_percent)
+        return fingers_percent
+
+
+if __name__ == '__main__':
+    finger_regressor = HandBentStatus()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img, draw=False)
+        if len(lmList) != 0:
+            tip_ids = [4, 8, 12, 16, 20]
+            # finger_regressor.selected_tip_demo(lmList, 4)
+            fingers_stats = finger_regressor.fingers_bent_percent(lmList)
+            print(fingers_stats)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+                    1, (255, 0, 0), 3)
+
+        cv2.imshow("Img", img)
+        cv2.waitKey(1)
